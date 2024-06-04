ec853e9f2c9f6bb2c7e81ee65b7293059b56a571
Anjisooo <jisoo_0718@naver.com>
2024-06-04 21:56:20 +0900

    test

--- miniOS/kernel/top/top.h
+++ miniOS/kernel/top/top.h
@@ -1,7 +1,7 @@
 /*  ***********************Include source**************************/
 // include 이유와 사용부분, 이름을 꼭 ! 기입해주세요.
 #include <stdio.h>
-#include <string.h>
+#include <string.h> // str을 읽어오기 위해 include 2행 - 지수
 #include <math.h> // isnan 사용하기 위해 include - 3행, 민석
 #include <ctype.h> // isdigit 사용하기 위해 include - 3행, 민석
 #include <curses.h> // mvprintw 사용하기 위해 include - 3행, 민석
@@ -29,9 +29,11 @@ unsigned int hertz;	 			//os의 hertz값이 저장된 변수 - 민석 [구현 �
 #define CPU_row 2
 #define b 3
 #define c 4
+#define TASK_ROW 1		//process state를 출력하고자하는 행 - 지수
+
 /*  ***********************프로세스 구조체**************************/
 typedef struct{
-	unsigned long pid;
+	unsigned long pid; 
 	unsigned long uid;			//USER 구하기 위한 uid
 	char user[32];		//user명
 	long double cpu;			//cpu 사용률
@@ -49,3 +51,57 @@ typedef struct{
 	char command[1024];		//option 있을 경우에 출력되는 command (long)
 	
 }myProc;
+
+
+#define PROCESS_MAX 4096 //process 최대 크기 정의 - 지수
+
+/********************* 길이 설정************************/ //2행, erase 구조체에서 사용
+#define PATH_LEN 1024
+#define UNAME_LEN 32
+#define TTY_LEN 32
+#define TOKEN_LEN 32
+#define STAT_LEN 8
+#define TIME_LEN 16
+#define CMD_LEN 1024
+
+/***************2행 /proc/pid/stat에서의 idx*************/
+#define STAT_PID_IDX 0
+#define STAT_TPGID_IDX 7
+#define STAT_NICE_IDX 18
+#define STAT_N_THREAD_IDX 19
+
+/******************6행 column index*******************/
+#define COLUMN_CNT 11 //출력할 column 최대 갯수
+
+#define PID_IDX 0
+#define PR_IDX 1
+#define NI_IDX 2
+#define VIRT_IDX 3
+#define RES_IDX 4
+#define SHR_IDX 5
+#define S_IDX 6
+#define CPU_IDX 7
+#define MEM_IDX 8
+#define TIME_P_IDX 9
+#define COMMAND_IDX 10
+
+// column에 출력할 문자열
+#define PID_STR "PID"
+#define PR_STR "PR"
+#define NI_STR "NI"
+#define VSZ_STR "VSZ"
+#define VIRT_STR "VIRT"
+#define RSS_STR "RSS"
+#define RES_STR "RES"
+#define SHR_STR "SHR"
+#define S_STR "S"
+#define STAT_STR "STAT"
+#define START_STR "START"
+#define TTY_STR "TTY"
+#define CPU_STR "%CPU"
+#define MEM_STR "MEM"
+#define TIME_STR "TIME"
+#define TIME_P_STR "TIME+"
+#define CMD_STR "CMD"
+#define COMMAND_STR "COMMAND"
+
--- miniOS/kernel/top/tophelp.c
+++ miniOS/kernel/top/tophelp.c
@@ -2,26 +2,184 @@
 extern myProc procList[4096];
 extern int procCnt;
 
-// 시스템 시작부터의 업타임을 가져오는 함수
-unsigned long get_uptime(void) {
-    FILE *fp;
-    char buf[BUFFER_SIZE];
-    long double time;
-
-    memset(buf, '\0', BUFFER_SIZE);
-
-    if ((fp = fopen(UPTIME, "r")) == NULL) {
-        fprintf(stderr, "fopen error for %s\n", UPTIME);
-        exit(1);
-    }
-    fgets(buf, BUFFER_SIZE, fp);
-    sscanf(buf, "%Lf", &time);
-    fclose(fp);
-
-    return (unsigned long)time;
+//proc의 내용을 지우는 함수
+void erase_proc(myProc *proc)
+{
+	proc->pid = 0;
+	proc->uid = 0;
+	memset(proc->user, '\0', UNAME_LEN);
+	proc->cpu = 0.0;
+	proc->mem = 0.0;
+	proc->vsz = 0;
+	proc->rss = 0;
+	proc->shr = 0;
+	proc->priority = 0;
+	proc->nice = 0;
+	memset(proc->tty, '\0', TTY_LEN);
+	memset(proc->stat, '\0', STAT_LEN);
+	memset(proc->start, '\0', TIME_LEN);
+	memset(proc->time, '\0', TIME_LEN);
+	memset(proc->cmd, '\0', CMD_LEN);
+	memset(proc->command, '\0', CMD_LEN);
+	return;
 }
 
-// 프로세스 정보를 검색하여 procList에 추가하는 함수
+// procList 내용 지우는 함수
+void erase_proc_list(void)
+{
+	for(int i = 0; i < procCnt; i++)
+		erase_proc(procList + i);
+	procCnt = 0;
+	return;
+}
+
+void add_proc_list(char path[1024], unsigned long cpuTimeTable[999999]) {
+    if (access(path, R_OK) < 0) {
+        fprintf(stderr, "access error for %s\n", path);
+        return;
+    }
+    myProc proc;
+    erase_proc(&proc);
+
+    char statPath[1024];
+    strcpy(statPath, path);
+    strcat(statPath, "/stat");
+
+    if (access(statPath, R_OK) < 0) {
+        fprintf(stderr, "access error for %s\n", statPath);
+        return;
+    }
+    FILE *statFp;
+    if ((statFp = fopen(statPath, "r")) == NULL) {
+        sleep(1);
+        return;
+    }
+
+    char statToken[32][32];
+    memset(statToken, '\0', 32 * 32);
+    for (int i = 0; i < 32; i++)
+        fscanf(statFp, "%s", statToken[i]);
+    fclose(statFp);
+
+    proc.pid = (long)atoi(statToken[0]); // pid 획득
+
+    // user명 획득
+    char statusPath[1024];
+    strcpy(statusPath, path);
+    strcat(statusPath, "/status");
+
+    if (access(statusPath, R_OK) < 0) {
+        fprintf(stderr, "access error for %s\n", statusPath);
+        return;
+    }
+    FILE *statusFp;
+    if ((statusFp = fopen(statusPath, "r")) == NULL) {
+        sleep(1);
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), statusFp)) {
+        if (strncmp(line, "Uid:", 4) == 0) {
+            unsigned long uid;
+            sscanf(line, "Uid:\t%lu", &uid);
+            proc.uid = uid;
+            struct passwd *pw = getpwuid(uid);
+            if (pw) {
+                strncpy(proc.user, pw->pw_name, UNAME_LEN - 1);
+                proc.user[UNAME_LEN - 1] = '\0'; // Ensure null-termination
+            } else {
+                strncpy(proc.user, "unknown", UNAME_LEN - 1);
+                proc.user[UNAME_LEN - 1] = '\0'; // Ensure null-termination
+            }
+            break;
+        }
+    }
+    fclose(statusFp);
+
+    // %cpu 계산
+    unsigned long utime = (unsigned long)atoi(statToken[13]);
+    unsigned long stime = (unsigned long)atoi(statToken[14]);
+    unsigned long startTime = (unsigned long)atoi(statToken[21]);
+    unsigned long long totalTime = utime + stime;
+
+    struct sysinfo sysInfo;
+    sysinfo(&sysInfo);
+    unsigned long long uptime = sysInfo.uptime;
+    unsigned long long Hertz = sysconf(_SC_CLK_TCK);
+
+    long double seconds = uptime - (startTime / Hertz);
+    long double cpuUsage = 100.0 * ((totalTime / Hertz) / seconds);
+
+    if (cpuUsage < 0 || cpuUsage > 100) {
+        proc.cpu = 0;
+    } else {
+        proc.cpu = roundl(cpuUsage * 100) / 100.0; // 소수점 2자리 반올림
+    }
+
+    // 메모리 사용률 계산
+    unsigned long vsz = 0, rss = 0, shr = 0, vmLck = 0;
+    if ((statusFp = fopen(statusPath, "r")) != NULL) {
+        while (fgets(line, sizeof(line), statusFp)) {
+            if (strncmp(line, "VmSize:", 7) == 0) {
+                sscanf(line, "VmSize:\t%lu", &vsz);
+            } else if (strncmp(line, "VmRSS:", 6) == 0) {
+                sscanf(line, "VmRSS:\t%lu", &rss);
+            } else if (strncmp(line, "RssShmem:", 9) == 0) {
+                sscanf(line, "RssShmem:\t%lu", &shr);
+            } else if (strncmp(line, "VmLck:", 6) == 0) {
+                sscanf(line, "VmLck:\t%lu", &vmLck);
+            }
+        }
+        fclose(statusFp);
+    }
+
+    proc.vsz = vsz;
+    proc.rss = rss;
+    proc.shr = shr;
+
+    long double memUsage = (long double)rss / sysInfo.totalram * 100.0;
+    if (memUsage < 0 || memUsage > 100) {
+        proc.mem = 0;
+    } else {
+        proc.mem = roundl(memUsage * 100) / 100.0; // 소수점 2자리 반올림
+    }
+
+    // priority 및 nice 값 획득
+    proc.priority = atoi(statToken[17]);
+    proc.nice = atoi(statToken[18]);
+
+    // START 획득
+    unsigned long start = time(NULL) - uptime + (startTime / Hertz);
+    struct tm *tmStart = localtime(&start);
+    if (time(NULL) - start < 24 * 60 * 60) {
+        strftime(proc.start, TIME_LEN, "%H:%M", tmStart);
+    } else if (time(NULL) - start < 7 * 24 * 60 * 60) {
+        strftime(proc.start, TIME_LEN, "%b %d", tmStart);
+    } else {
+        strftime(proc.start, TIME_LEN, "%y", tmStart);
+    }
+
+    // TIME 획득
+    unsigned long cpuTime = totalTime / Hertz;
+    struct tm *tmCpuTime = localtime(&cpuTime);
+    if (!isPPS || (!aOption && !uOption && !xOption)) { // ttop이거나 pps에서 옵션이 없을 경우
+        sprintf(proc.time, "%02d:%02d:%02d", tmCpuTime->tm_hour, tmCpuTime->tm_min, tmCpuTime->tm_sec);
+    } else {
+        sprintf(proc.time, "%1d:%02d", tmCpuTime->tm_min, tmCpuTime->tm_sec);
+    }
+
+    // command 획득
+    sscanf(statToken[1], "(%s", proc.cmd); // cmd 획득
+    proc.cmd[strlen(proc.cmd) - 1] = '\0'; // 마지막 ')' 제거
+
+    // procList에 정보 저장
+    procList[procCnt] = proc;
+    procCnt++;
+
+    return;
+}
+
 void get_procpath(unsigned long cpuTimeTable[999999]) {
     DIR *dirPtr;
     if ((dirPtr = opendir("/proc")) == NULL) { // /proc 디렉터리 open
@@ -52,56 +210,5 @@ void get_procpath(unsigned long cpuTimeTable[999999]) {
         add_proc_list(path, cpuTimeTable); // PID 디렉터리인 경우 procList에 추가
     }
     closedir(dirPtr);
-    return;
-}
-
-// 프로세스 정보를 구조체에 저장하고 procList에 추가하는 함수
-void add_proc_list(char path[1024], unsigned long cpuTimeTable[999999]) {
-    if (access(path, R_OK) < 0) {
-        fprintf(stderr, "access error for %s\n", path);
-        return;
-    }
-    myProc proc;
-    erase_proc(&proc);
-
-    char statPath[1024];
-    strcpy(statPath, path);
-    strcat(statPath, "/stat");
-
-    if (access(statPath, R_OK) < 0) {
-        fprintf(stderr, "access error for %s\n", statPath);
-        return;
-    }
-    FILE *statFp;
-    if ((statFp = fopen(statPath, "r")) == NULL) {
-        // fprintf(stderr, "fopen error %s %s\n", strerror(errno), statPath);
-        sleep(1);
-        return;
-    }
-
-    char statToken[32][32];
-    memset(statToken, '\0', 32 * 32);
-    for (int i = 0; i < 32; i++)
-        fscanf(statFp, "%s", statToken[i]);
-    fclose(statFp);
-
-    proc.pid = (long)atoi(statToken[0]); // pid 획득
-
-    // user명 획득
-
-    // %cpu 계산
-
-    // priority 및 nice 값 획득
-
-    // START 획득
-
-    // TIME 획득
-
-    // command 획득
-
-    // procList에 정보 저장
-    procList[procCnt].pid = proc.pid;
-    procCnt++;
-
     return;
 }
